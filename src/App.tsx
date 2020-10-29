8b404e458966d8e1a514f3e05421766c63477c70
shinichiro <bosse.de.poulet@gmail.com>
2020-10-29 20:01:50 +0900

    Delete unused files and copy HTML from firebase template

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,126 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header
+        className="mdl-layout__header mdl-color-text--white mdl-color--light-blue-700"
+      >
+        <div className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
+          <div
+            className="mdl-layout__header-row mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--8-col-desktop"
+          >
+            <h3>freee sample app on firebase</h3>
+          </div>
+        </div>
       </header>
+      <div id="loading-container" className="loading">
+        <div
+          className="mdl-spinner mdl-js-spinner is-active"
+          style={{width: "80px", height: "80px"}}
+        ></div>
+        <div>ユーザー情報読み込み中</div>
+      </div>
+      <main id="main-container" className="mdl-layout__content mdl-color--grey-100">
+        <div id="demo-signed-in-card" className="mdl-grid">
+          <div className="mdl-card mdl-shadow--2dp mdl-cell--12-col">
+            <div className="mdl-card__supporting-text mdl-color-text--grey-600">
+              <p>
+                ようこそ <span id="demo-name-container"></span>さん<br />
+                Freee User ID: <span id="demo-uid-container"></span><br />
+              </p>
+              <div>
+                <div
+                  id="companies-select"
+                  className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label mdl-select"
+                >
+                  <input
+                    className="mdl-textfield__input"
+                    type="text"
+                    id="select-section"
+                  />
+                  <label className="mdl-textfield__label" htmlFor="sample3"
+                    >現在の事業所</label
+                  >
+                  <ul
+                    id="companies-select-ul"
+                    className="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect mdl-js-ripple-effect--ignore-events"
+                  ></ul>
+                </div>
+              </div>
+
+              <div>
+                <div
+                  id="types-select"
+                  className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label mdl-select"
+                >
+                  <input
+                    className="mdl-textfield__input"
+                    type="text"
+                    id="types-select-section"
+                  />
+                  <label className="mdl-textfield__label" htmlFor="sample3"
+                    >収支区分 </label
+                  >
+                  <ul
+                    id="types-select-ul"
+                    className="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect mdl-js-ripple-effect--ignore-events"
+                  ></ul>
+                </div>
+              </div>
+
+              <div>
+                <div
+                  id="account_items-select"
+                  className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label mdl-select"
+                >
+                  <input
+                    className="mdl-textfield__input"
+                    type="text"
+                    id="account_items-select-section"
+                  />
+                  <label className="mdl-textfield__label" htmlFor="account_items-select-section"
+                    >勘定科目</label
+                  >
+                  <ul
+                    id="account_items-select-ul"
+                    className="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect mdl-js-ripple-effect--ignore-events"
+                  ></ul>
+                </div>
+              </div>
+              <div>
+                <div
+                  id="amount-input"
+                  className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"
+                >
+                  <input
+                    className="mdl-textfield__input"
+                    type="text"
+                    id="amount-input-section"
+                  />
+                  <label className="mdl-textfield__label" htmlFor="amount-input-section"
+                    >金額</label
+                  >
+                </div>
+              </div>
+
+              <button
+                id="demo-post-deal-button"
+                className="mdl-button--primary mdl-button--raised mdl-button mdl-js-button mdl-js-ripple-effect"
+              >
+                取引をPOST
+              </button>
+              <button
+                id="demo-sign-out-button"
+                className="mdl-color-text--grey-700 mdl-button--raised mdl-button mdl-js-button mdl-js-ripple-effect"
+                style={{marginLeft: "8px"}}
+              >
+                ログアウト
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
